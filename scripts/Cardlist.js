0accf3e04a0af7908f577411e235c1007ac1e5ee
Andrei Rassanov <totsamiyandrey@gmail.com>
2020-08-25 07:39:09 +0400

    My original code

---
+++ scripts/Cardlist.js
@@ -0,0 +1,21 @@
+class CardList{
+
+	constructor (container, createCard, openPicture) {
+
+		this.container = container;
+		this.openPicture = openPicture;
+		this.createCard = createCard;
+
+	} 
+    
+	addCard (card) {
+		let cardNode = this.createCard(this.openPicture).create(card); 
+        this.container.appendChild(cardNode);
+	}
+
+	render (initialArray) {
+		this.initialArray = initialArray;
+		this.initialArray.forEach(card => this.addCard(card));
+    }
+
+}
